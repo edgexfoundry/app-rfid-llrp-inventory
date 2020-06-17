4c92b62de58c80b18c11624b31d743564447173d
Surajit Pal <surajitx.pal@intel.com>
2020-06-17 13:23:09 -0700

    Clean and update - Jenkins build wokring
    Signed-off-by: Surajit Pal <surajitx.pal@intel.com>

--- tests/utils/rmvDockerImgs.sh
+++ tests/utils/rmvDockerImgs.sh
@@ -3,13 +3,10 @@
 # Copyright (C) 2020 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+# Remove all EDGEX-FOUNDRY images except device-llrp-go
+$(docker images -a | grep  -v "device-llrp-go" | grep "edgexfoundry" | awk '{print $3}' | xargs docker rmi -f)
 
-#echo "Remove all EDGEX-FOUNDRY images"
-#$(docker images -a  | grep "edgexfoundry" | awk '{print $3}' | xargs docker rmi -f)
-$(docker images -a | grep  -v "docker-device-llrp-go" | grep "edgexfoundry" | awk '{print $3}' | xargs docker rmi -f)
-
-echo "Remove unused containers resources then remove dangling images"
-
+# Stop/Remove unused containers resources then remove dangling images and volume
 $(docker rm -v $(docker ps -aqf status=exited))
 $(docker rmi $(docker images -qf dangling=true))
 $(docker volume rm $(docker volume ls -qf dangling=true))
