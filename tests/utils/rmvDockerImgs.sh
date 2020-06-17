c11ceb9841a3523980aabdf54ee88703a2ce2471
Surajit Pal <surajitx.pal@intel.com>
2020-06-17 13:08:04 -0700

    Jenkins build debugging -1
    Signed-off-by: Surajit Pal <surajitx.pal@intel.com>

--- tests/utils/rmvDockerImgs.sh
+++ tests/utils/rmvDockerImgs.sh
@@ -4,8 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-echo "Remove all EDGEX-FOUNDRY images"
-$(docker images -a  | grep "edgexfoundry" | awk '{print $3}' | xargs docker rmi -f)
+#echo "Remove all EDGEX-FOUNDRY images"
+#$(docker images -a  | grep "edgexfoundry" | awk '{print $3}' | xargs docker rmi -f)
+$(docker images -a | grep  -v "docker-device-llrp-go" | grep "edgexfoundry" | awk '{print $3}' | xargs docker rmi -f)
 
 echo "Remove unused containers resources then remove dangling images"
 
